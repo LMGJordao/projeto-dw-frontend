1839aff6cf8b490c128b8196a99634d37ca77705
webdevdeiuc <152176228+webdevdeiuc@users.noreply.github.com>
2023-11-27 18:34:54 +0000

    update api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 //export const API_URL = 'https://portfolio.humaginarium.net/wp-json/wp/v2/';
-export const API_URL = 'https://webdevdeiuc.kesug.com/wp-json/wp/v2/';
+export const API_URL = 'https://cancerous-upside.000webhostapp.com/wp-json/wp/v2/';
