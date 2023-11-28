da416486355e476ada77365ea19707f1cc0e080d
webdevdeiuc <152176228+webdevdeiuc@users.noreply.github.com>
2023-11-28 13:11:26 +0000

    update api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-//export const API_URL = 'https://portfolio.humaginarium.net/wp-json/wp/v2/';
-export const API_URL = 'https://unmatriculated-flus.000webhostapp.com//wp-json/wp/v2/';
+export const API_URL = 'https://portfolio.humaginarium.net/wp-json/wp/v2/';
+//export const API_URL = 'https://unmatriculated-flus.000webhostapp.com//wp-json/wp/v2/';
